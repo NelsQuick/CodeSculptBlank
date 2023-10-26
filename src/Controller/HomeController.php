243a421e6d5bc139eee0a5ae67eb000afa7e2a62
NelsQuick <nelsonpires.pro@gmail.com>
2023-10-26 19:07:03 +0200

    OnlyUserAccess

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(AuthorizationCheckerInterface $authChecker): Response
     {
+        // Vérifie si l'utilisateur est connecté
+        if (!$authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            // Redirige l'utilisateur vers une page de connexion
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
